846bd898b7ace77b147d105b5cc44fc745ab7fd0
atuldemond <atuldemond@gmail.com>
2024-08-10 12:15:01 +0530

    backend

--- app.js
+++ app.js
@@ -73,5 +73,5 @@ app.all("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port 3000 ${PORT}`);
+  console.log(`Server is running on port  ${PORT}`);
 });
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const connectionDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(
+      "mongodb+srv://atuldemond:3oXRQuxdtP8AnsNJ@khatabook.fvh9rot.mongodb.net/?retryWrites=true&w=majority&appName=khatabook"
+    );
     console.log("Connected TO DB");
   } catch (error) {
     console.error("unable to connect to mongoose");
