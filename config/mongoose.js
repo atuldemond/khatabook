6ca8b9459a50555404ee3231b0d4993129253053
atuldemond <atuldemond@gmail.com>
2024-08-10 12:30:46 +0530

    backend

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const connectionDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://atuldemond:3oXRQuxdtP8AnsNJ@khatabook.fvh9rot.mongodb.net/?retryWrites=true&w=majority&appName=khatabook"
-    );
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("Connected TO DB");
   } catch (error) {
     console.error("unable to connect to mongoose");
