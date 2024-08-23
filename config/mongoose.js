75980e4e8ad872eceb1997142ee669d0803ebe77
da2323 <atuldemond@gmail.com>
2024-08-23 05:36:43 +0530

    v-2

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 const connectionDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
     console.log("Connected TO DB");
   } catch (error) {
     console.error("unable to connect to mongoose");
