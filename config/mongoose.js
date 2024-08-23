4b3566b79e9a4a8de3730fc733112ae49702064f
da2323 <atuldemond@gmail.com>
2024-08-23 06:33:39 +0530

    v-2.4

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 const connectionDB = async () => {
  
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb+srv://atuldemond:3oXRQuxdtP8AnsNJ@khatabook.fvh9rot.mongodb.net/?retryWrites=true&w=majority&appName=khatabook")
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Failed to connect to MongoDB:', err.message));
 
