567e0afe7b219fc3c107df6abd68785476144f87
da2323 <atuldemond@gmail.com>
2024-08-23 06:50:34 +0530

    v-2.5

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectionDB = async () => {
  
 mongoose.connect("mongodb+srv://atuldemond:3oXRQuxdtP8AnsNJ@khatabook.fvh9rot.mongodb.net/?retryWrites=true&w=majority&appName=khatabook")
-.then(() => console.log('Connected to MongoDB'))
+.then(() => console.log('Connected to MongoDB, its will for gloabl'))
 .catch(err => console.error('Failed to connect to MongoDB:', err.message));
 
 };
